92441f0ae165fb902c7db9d9e4e2d2b9062a60b7
benwk <z@benwk.com>
2021-11-24 00:43:28 +0800

    :tada: (project) init the project

---
+++ README.md
@@ -0,0 +1,7 @@
+English | [中文版](README-zh.md "README 中文版")
+
+# benwk is coding
+
+benwk's website, thanks for reading.
+
+[https://benwk.com](https://benwk.com "benwk is coding")
---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+
+const title = 'benwk\'s coding'
+const author = 'benwk'
+const domain = 'https://benwk.com'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>{ title }</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Welcome to{' '}
+          <br />
+          <br />
+          <a className="text-blue-600" href={ domain }>
+            { title }
+          </a>
+        </h1>
+      </main>
+
+      <footer className="flex flex-col items-center justify-center w-full h-24 border-t">
+        <div>
+          Designed and built{' '}with ❤️ by{' '}
+          <a className="text-blue-600 font-semibold" href="mailto:z@benwk.com">
+            { author }
+          </a>
+        </div>
+        
+      </footer>
+    </div>
+  )
+}
