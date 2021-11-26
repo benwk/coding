ac535d1fe41fef47adf5ca0cc8808809eee66bf3
benwk <z@benwk.com>
2021-11-26 23:57:25 +0800

    :ambulance: (sidebar) fix flex problems

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div className="text-white h-full">
-      <h1>Sidebar</h1>
-      <div className="flex flex-wrap content-center justify-center h-full">
-        Hello World
+    <div className="text-white h-full flex flex-col">
+      <div>
+        <h1>Sidebar</h1>
+      </div>
+      <div className="flex items-center justify-center flex-grow">
+        <div className="bg-red-500 w-52 h-52 flex flex-wrap content-center justify-center">Hello World</div>
       </div>
     </div>
   )
