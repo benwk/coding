bd4c5226cefc1f242972f6cbee7f9c5662ffcd28
benwk <z@benwk.com>
2021-12-06 23:13:19 +0800

    :ambulance: (name) change sidebar to navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Header } from '.'
 import { Footer } from '.'
-import { Sidebar } from '.'
+import { Navbar } from '.'
 
 const Layout = ({ children }) => {
   return (
     <div className="flex lg:flex-row sm:flex-col flex-col-reverse min-h-screen">
       <div className="sticky sm:top-0 bottom-0 lg:w-1/3">
         {/* TODO: why sticky needs to be defined twice? */}
-        <Sidebar />
+        <Navbar />
       </div>
       <div className="flex flex-grow items-center justify-center">
         {/* <Header /> */}
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Sidebar = () => {
+const Navbar = () => {
   return (
     <div className="bg-black text-white sticky top-0 lg:h-screen flex flex-col">
       <div>
-        <h1>Sidebar</h1>
+        <h1>Navbar</h1>
       </div>
       <div className="flex items-center justify-center flex-grow">
         <div className="hidden bg-red-500 w-52 h-52 lg:flex flex-wrap content-center justify-center">Hello World</div>
@@ -13,4 +13,4 @@ const Sidebar = () => {
   )
 }
 
-export default Sidebar
+export default Navbar
