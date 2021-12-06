ed2521cd2f94b2bff9dec7fd2576712b225d1043
benwk <z@benwk.com>
2021-12-06 23:29:31 +0800

    :art: (test responsive layout)

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,6 +7,8 @@ const Navbar = () => {
         <h1>Navbar</h1>
       </div>
       <div className="flex items-center justify-center flex-grow">
+        <div className="hidden sm:block lg:hidden bg-green-500 w-52 h-52"></div>
+        <div className="sm:hidden bg-blue-500 w-52 h-52"></div>
         <div className="hidden bg-red-500 w-52 h-52 lg:flex flex-wrap content-center justify-center">Hello World</div>
       </div>
     </div>
